435f96454a01a5dac15eb013d109b89c019a45b3
Mayank Jaiswal <84842694+mayankmj25@users.noreply.github.com>
2023-03-20 11:44:01 +0530

    Delete helper.js

--- helper.js
+++
@@ -1,6 +0,0 @@
-export const createElement = (elementType, elementClassName, elementId) => {
-  const element = document.createElement(elementType);
-  element.setAttribute("class", elementClassName);
-  element.setAttribute("id", elementId);
-  return element;
-};
