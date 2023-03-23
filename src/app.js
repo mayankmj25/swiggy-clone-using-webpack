ad26ce24920c57a8a25beb960f9c316648767662
itsakarya <aniket2018pr@gmail.com>
2023-03-23 13:48:55 +0530

    header and footer modified

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import { createMainHeader } from "./header/index.js";
-// import breadCrumb from "./breadCrumbList/index.js";
-// import restaurentDiv from "./restaurent/index.js";
+import {breadCrumb} from "./breadCrumbList/index.js";
+import {restaurentDiv} from "./restaurent/index.js";
 // import { createMenuSection } from "./menuInfo/index.js";
 import { createFooter } from "./footer/index.js";
 
 // document.body.appendChild(breadCrumb);
 // document.body.appendChild(restaurentDiv);
 document.body.append(createMainHeader());
+breadCrumb();
+restaurentDiv();
 document.body.append(createFooter());
 // document.body.append(createMenuSection());
